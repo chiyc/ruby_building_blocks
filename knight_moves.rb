d78454001eea513ac9c6125aa712843e2c1c6c1a
Chi <cychoi@zoho.com>
2017-01-24 13:40:57 -0700

    finish working knight_moves.rb

--- knight_moves.rb
+++ knight_moves.rb
@@ -1,4 +1,32 @@
+def coord_to_key(coord)
+  return coord[0] + 10*coord[1]
+end
 
+def key_to_coord(key)
+  col = key % 10
+  row = key / 10
+  return [col,row]
+end
+
+def is_possible?(p)
+  possible = true
+
+  if p.class == Fixnum
+    col = p % 10
+    row = p / 10
+  elsif p.class == Array
+    col = p[0] % 10
+    row = p[1] / 10
+  else
+    possible = false
+  end
+
+  if col > 7 || row < 0 || row > 7
+    possible = false
+  end
+
+  return possible
+end
 
 def possible_positions_from(position)
   possible_positions = [[position[0]-2,position[1]-1],[position[0]-2,position[1]+1],[position[0]+2,position[1]-1],[position[0]+2,position[1]+1],[position[0]-1,position[1]-2],[position[0]-1,position[1]+2],[position[0]+1,position[1]-2],[position[0]+1,position[1]+2]]
@@ -9,83 +37,56 @@ def possible_positions_from(position)
 end
 
 def knight_moves(start, finish)
-  moves = Hash.new([])
-  visited = Hash.new(false)
+  moves = Array.new
+  visited = Array.new
   populating_graph = true
   queue = [start]
   while populating_graph
-    position = Object.new
-    position = queue.shift
-
- #   puts "We just shifted off #{position}"
-#    p position
-    visited[position.clone] = true
-    possible_positions = possible_positions_from(position)
+    position = coord_to_key(queue.shift)
+    if moves[position] == nil
+      moves[position] = []
+    end
+    visited[position] = true
+    possible_positions = possible_positions_from(key_to_coord(position))
 
     possible_positions.each do |p|
-      if visited[p] == false
- #       puts "Pushing #{p} onto hash for #{position}"
-        moves[position.clone].push(p)
-#        p moves[position]
-        visited[p] = true
+      if visited[coord_to_key(p)] == nil
+        moves[position].push(coord_to_key(p))
+        visited[coord_to_key(p)] = true
         queue.push(p)
-        if p == finish
+   #     puts "We pushed #{coord_to_key(p)} to the starting position of #{position}"
+        if coord_to_key(p) == coord_to_key(finish)
           populating_graph = false
         end # if
       end # if
     end # do
-    puts "End while"
- #   p moves.keys
- #   p moves[position]
 
   end # while
 
-
-
-
-
   path = []
-  node = nil
-  last = finish
-=begin
-  until last == start
-    puts "Entered until loop"
-    moves.each_value do |value|
-      if value.include?(last)
-        puts "Found that #{last}"
-        path.unshift(last)
-        last = moves.key(value)
+  last = coord_to_key(finish)
+  jumps = 0
+  until last == coord_to_key(start)
+#    puts "Searching for origin of #{last}"
+    moves.each_with_index do |paths, key|
+      if paths != nil
+        if paths.include?(last)
+  #        puts "Found #{last} came from #{key}"
+          path.unshift(key_to_coord(last))
+          last = key
+          jumps += 1
+        end
       end
     end
   end
-
-
-
-=begin
-  visited = Hash.new(false)
-  queue = [start]
-  path = []
-  path.push(start)
-  while true
-    current = queue.shift
-    visited[current] = true
-    if current == finish
-      break
-    end
-
-    moves[current].each do |move|
-      queue.push(move)
-    end
+  path.unshift(start)
+  puts "You made it in #{jumps} moves! Here's your path:"
+  path.each do |key|
+    p key
   end
-=end
-
-  return moves
+  return path
 end
 
-
-
-
-node = knight_moves([3,3],[4,3])
-puts "#{node[[3,3]]}"
+knight_moves([3,3],[4,3])
 puts
-puts "#{possible_positions_from([3,3])}"
+puts knight_moves([0,0],[1,2]) == [[0,0],[1,2]]
